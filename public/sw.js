459cb44e67a7b0c38accb869f17c5b7369ad7d3f
idonutyou <whwpals1414@gmail.com>
2026-09-01 23:57:59 +0000

    Match family selector size to payroll inputs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cat-attendance-v192";
+const CACHE_NAME = "cat-attendance-v193";
 
 const APP_SHELL = [
   "./",
--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ import "./v185-overrides.css";
 import "./v186-overrides.css";
 import "./v190-overrides.css";
 import "./v191-overrides.css";
+import "./v193-overrides.css";
 import { firebaseConfig } from "./firebase-config.js";
 
 const STORAGE_KEY = "cat-attendance-records-v1";
