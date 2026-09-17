df4e34a6596f06590ed803a0f1317cdd5bf7f744
idonutyou <whwpals1414@gmail.com>
2026-09-17 08:24:54 +0000

    Enable direct widget update via GitHub release v200

--- src/main.js
+++ src/main.js
@@ -72,7 +72,7 @@ const APP_PAGE_TITLES = {
 const FIXED_SAFETY_ALLOWANCE = 50000;
 
 const WIDGET_INSTALL_URL_KEY = "cat-widget-install-url-v1";
-const DEFAULT_WIDGET_INSTALL_URL = "";
+const DEFAULT_WIDGET_INSTALL_URL = "https://github.com/idonutyou/cat-attendance/releases/download/widget-latest/CAT-widget.apk";
 const widgetInstallImageUrl = `${import.meta.env.BASE_URL}android-widget-user.png`;
 
 const CAT_BROWSER_ENV = (() => {
